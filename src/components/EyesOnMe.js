6ef484e3b7904fce668c6d1173cae5ccf6639386
N M <niclas@mettenleiter.com>
2019-08-17 11:31:49 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -5,10 +5,13 @@ class EyesOnMe extends React.Component {
   const focusHandler = function {
     console.log('Good!')
   }
+  const blurHandler = function {
+    console.log('Hey! Eyes on me!')
+  }
 
   render () {
     return (
-      <button onFocus={this.} onBlur={this.}
+      <button onFocus={this.focusHandler} onBlur={this.blurHandler}
     )
   }
 }
