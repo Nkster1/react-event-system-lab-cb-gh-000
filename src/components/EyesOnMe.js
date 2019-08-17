fd1de996ec32d05bad462a94d406dc0962b83e36
N M <niclas@mettenleiter.com>
2019-08-17 11:31:11 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,2 +1,14 @@
 // Code EyesOnMe Component Here
 import  React from "react";
+
+class EyesOnMe extends React.Component {
+  const focusHandler = function {
+    console.log('Good!')
+  }
+
+  render () {
+    return (
+      <button onFocus={this.} onBlur={this.}
+    )
+  }
+}
