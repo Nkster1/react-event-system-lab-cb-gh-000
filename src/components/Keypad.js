34b24ea5513c04e26984d155d6b322595e517549
N M <niclas@mettenleiter.com>
2019-08-17 11:27:03 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,2 +1,8 @@
 // Code Keypad Component Here
 import React from "react";
+
+class Keypad extends React.Component{
+  const enterPassword = function {
+    console.log('Entering password...')
+  }
+}
